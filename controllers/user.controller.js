0bf173c02b9e6a7ed77d942f8fec753f5fb54df0
Canda13 <candelaalfonso13@gmail.com>
2025-08-04 15:27:36 -0300

    agrego los partials

---
+++ controllers/user.controller.js
@@ -0,0 +1,11 @@
+module.exports ={
+    login:function (req,res,next){
+        res.render("login.ejs");
+    },
+};
+
+module.exports ={
+    register:function (req,res,next){
+        res.render("register.ejs");
+    },
+};
